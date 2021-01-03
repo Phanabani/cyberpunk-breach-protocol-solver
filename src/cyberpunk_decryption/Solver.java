72e8e8c89097d1b6abc363a8078cf6d82a47dc01
Hawkpath <hawkpathas@gmail.com>
2021-01-03 15:42:14 -0500

    Add buffer overflow check and "bridging" functionality (reach a sequence by way of a "bridge" cell). Bridging erroneously occurs in the middle of a sequence though, this must be fixed.

--- src/cyberpunk_decryption/Solver.java
+++ src/cyberpunk_decryption/Solver.java
@@ -117,14 +117,19 @@ public class Solver {
     // This is a recursive problem.
     ArrayDeque<SolutionNode> stack = new ArrayDeque<>();
     Integer[] seq = sequences[sequences.length - 1];
-    boolean solved = solveRecursive(stack, seq, 0, 0);
+    boolean solved = solveRecursive(stack, seq, 0, 0, 0);
     solution = solved ? stack.toArray(new SolutionNode[0]) : null;
   }
 
   private boolean solveRecursive(
-      Deque<SolutionNode> deque, Integer[] sequence, int seqIndex, int rowOrCol
+      Deque<SolutionNode> deque, Integer[] sequence,
+      int bufferIndex, int seqIndex, int rowOrCol
   ) {
-    boolean horizontal = seqIndex % 2 == 0;
+    if (sequence.length - seqIndex + bufferIndex > bufferSize)
+      // It's impossible to finish this sequence without overflowing the buffer
+      return false;
+
+    boolean horizontal = bufferIndex % 2 == 0;
 
     if (horizontal) {
       for (
@@ -138,31 +143,51 @@ public class Solver {
           // Add to the solution stack and collapse back up the call stack.
           deque.push(new SolutionNode(x, rowOrCol, sequence[seqIndex]));
           return true;
-        } else if (solveRecursive(deque, sequence, seqIndex + 1, x)) {
+        } else if (solveRecursive(deque, sequence, bufferIndex+1, seqIndex+1, x)) {
           // We searched recursively and found everything.
           // Add to the solution stack and collapse up.
           deque.push(new SolutionNode(x, rowOrCol, sequence[seqIndex]));
           return true;
         }
       }
+
+      for (int x = 0; x < data.getWidth(); x++) {
+        // We didn't find anything in this row, so just use cells in this row
+        // as a bridge to get to the right value
+        // Do NOT increment seqIndex because we didn't actually find the value here
+        if (solveRecursive(deque, sequence, bufferIndex+1, seqIndex, x)) {
+          deque.push(new SolutionNode(x, rowOrCol, data.get(x, rowOrCol)));
+          return true;
+        }
+      }
     }
 
-    else {
+    else {  // vertical
       for (
           int y = data.findInColumn(rowOrCol, sequence[seqIndex], 0);
           y != -1 && y < data.getHeight();
           y = data.findInColumn(rowOrCol, sequence[seqIndex], y+1)
       ) {
-        // Iterate through all instances of value in this row
+        // Iterate through all instances of value in this column
         if (seqIndex == sequence.length - 1) {
           // We reached the end of the sequence and found everything.
           // Add to the solution stack and collapse back up the call stack.
           deque.push(new SolutionNode(rowOrCol, y, sequence[seqIndex]));
           return true;
-        } else if (solveRecursive(deque, sequence, seqIndex + 1, y)) {
+        } else if (solveRecursive(deque, sequence, bufferIndex+1, seqIndex+1, y)) {
           // We searched recursively and found everything.
           // Add to the solution stack and collapse up.
           deque.push(new SolutionNode(rowOrCol, y, sequence[seqIndex]));
+          return true;
+        }
+      }
+
+      for (int y = 0; y < data.getHeight(); y++) {
+        // We didn't find anything in this col, so just use cells in this col
+        // as a bridge to get to the right value
+        // Do NOT increment seqIndex because we didn't actually find the value here
+        if (solveRecursive(deque, sequence, bufferIndex+1, seqIndex, y)) {
+          deque.push(new SolutionNode(rowOrCol, y, data.get(rowOrCol, y)));
           return true;
         }
       }
