7c8140bad70a97340fdcebf4a29ce8d11eb9a2d5
Hawkpath <hawkpathas@gmail.com>
2021-01-07 05:29:58 -0500

    Add ArrayList -> array methods.

--- src/main/java/com/github/hawkpath/cyberpunk_breach_solver/Utils.java
+++ src/main/java/com/github/hawkpath/cyberpunk_breach_solver/Utils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Utils {
 
@@ -16,6 +17,39 @@ public class Utils {
     } catch (URISyntaxException e) {
       return null;
     }
+  }
+
+  public static Integer[][] toJaggedArray(ArrayList<ArrayList<Integer>> list) {
+    if (list.size() == 0)
+      return null;
+
+    Integer[][] outArr = new Integer[list.size()][];
+    for (int i=0; i<list.size(); i++) {
+      outArr[i] = list.get(i).toArray(new Integer[0]);
+    }
+
+    return outArr;
+  }
+
+  public static Integer[][] tryGet2DSubarray(ArrayList<ArrayList<Integer>> list) {
+    if (list.size() == 0)
+      return null;
+
+    int rowLen = list.get(0).size();
+    int sameSizedRows = 0;
+    for ( ; sameSizedRows<list.size(); sameSizedRows++) {
+      if (list.get(sameSizedRows).size() != rowLen)
+        break;
+    }
+    if (list.size() - sameSizedRows > 2)
+      return null;
+
+    Integer[][] outArr = new Integer[sameSizedRows][];
+    for (int i=0; i<sameSizedRows; i++) {
+      outArr[i] = list.get(i).toArray(new Integer[0]);
+    }
+
+    return outArr;
   }
 
 }
