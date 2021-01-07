2a8c4b7866a59d0582cb10b38d509f5f3aeeb291
Hawkpath <hawkpathas@gmail.com>
2021-01-06 23:30:21 -0500

    Handle text parsing in this class. Use Levenshtein distance to clean up any weird entries (we have a list of expected values, so anything that doesn't match should be matched to them).

--- src/main/java/com/github/hawkpath/cyberpunk_breach_solver/Detector.java
+++ src/main/java/com/github/hawkpath/cyberpunk_breach_solver/Detector.java
@@ -3,18 +3,20 @@ package com.github.hawkpath.cyberpunk_breach_solver;
 import net.sourceforge.tess4j.ITessAPI;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-class OCRResult {
-  String text;
+class DetectionResult {
+  ArrayList<ArrayList<Integer>> values;
   ArrayList<Rectangle> regions;
 
-  public OCRResult(String text, ArrayList<Rectangle> regions) {
-    this.text = text;
+  public DetectionResult(ArrayList<ArrayList<Integer>> values, ArrayList<Rectangle> regions) {
+    this.values = values;
     this.regions = regions;
   }
 }
@@ -24,10 +26,12 @@ public class Detector {
   private static final Rectangle basisRect = new Rectangle(0, 0, 2560, 1440);
   private static final Rectangle basisMatrixRect = new Rectangle(375, 450, 566, 510);
   // 3 sequences; might be taller downwards for more
-  private static final Rectangle basisSequencesRect = new Rectangle(1155, 450, 384, 304);
+  private static final Rectangle basisSequencesRect = new Rectangle(1100, 450, 384, 80);
   // 6 buffer slots; might be longer to the right
   private static final Rectangle basisBufferRect = new Rectangle(1143, 241, 364, 84);
-  private static final String[] possibleCells = {"FF", "55", "1C", "BD", "E9", "7A"};
+  private static final ArrayList<String> possibleCells = new ArrayList<>(Arrays.asList(
+      "FF", "55", "1C", "BD", "E9", "7A"
+  ));
 
   private static final int THRESHOLD = 100;
 
@@ -73,7 +77,7 @@ public class Detector {
     return new Rectangle(x, y, w, h);
   }
 
-  private OCRResult doOCR(BufferedImage img) {
+  private DetectionResult doOCR(BufferedImage img) {
     try {
       ImageProcessing.threshold(img, THRESHOLD);
       ImageProcessing.invert(img);
@@ -81,26 +85,70 @@ public class Detector {
       ArrayList<Rectangle> regions = (ArrayList<Rectangle>) tess.getSegmentedRegions(
           img, ITessAPI.TessPageIteratorLevel.RIL_WORD
       );
-      return new OCRResult(text, regions);
+      return new DetectionResult(parseText(text), regions);
     } catch (TesseractException e) {
       return null;
     }
   }
 
-  private OCRResult detect(Rectangle captureRegion) {
-    BufferedImage capture = robot.createScreenCapture(captureRegion);
-    OCRResult ocrResult = doOCR(capture);
-    if (ocrResult == null)
-      return null;
-    offsetRegions(ocrResult.regions, captureRegion.getLocation());
-    return ocrResult;
+  private static ArrayList<ArrayList<Integer>> parseText(String text) {
+    ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
+    for (String rowText : text.split("\n")) {
+      // Split up lines
+      ArrayList<Integer> row = new ArrayList<>();
+      for (String word : rowText.split(" ")) {
+        // Parse each word in the line as a hex value
+        row.add(parseWord(word));
+      }
+      rows.add(row);
+    }
+    return rows;
   }
 
-  public OCRResult detectMatrix() {
+  private static Integer parseWord(String word) {
+    if (!possibleCells.contains(word)) {
+      word = findNearestPossible(word);
+    }
+    try {
+      return Integer.parseInt(word, 16);
+    } catch (NumberFormatException e) {
+      return -1;
+    }
+  }
+
+  /**
+   * Find the nearest string by Levenshtein distance
+   */
+  private static String findNearestPossible(String word) {
+    LevenshteinDistance leven = new LevenshteinDistance(4);
+
+    int lowestDist = 0;
+    String bestMatch = null;
+    for (String possible : possibleCells) {
+      Integer dist = leven.apply(possible, word);
+      if (bestMatch == null || dist < lowestDist) {
+        lowestDist = dist;
+        bestMatch = possible;
+      }
+    }
+
+    return bestMatch;
+  }
+
+  private DetectionResult detect(Rectangle captureRegion) {
+    BufferedImage capture = robot.createScreenCapture(captureRegion);
+    DetectionResult detectionResult = doOCR(capture);
+    if (detectionResult == null)
+      return null;
+    offsetRegions(detectionResult.regions, captureRegion.getLocation());
+    return detectionResult;
+  }
+
+  public DetectionResult detectMatrix() {
     return detect(screenMatrixRect);
   }
 
-  public OCRResult detectSequences() {
+  public DetectionResult detectSequences() {
     return detect(screenSequencesRect);
   }
 
